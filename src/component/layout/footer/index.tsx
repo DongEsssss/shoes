283f5966eaada7f7bd2816e7f440e1469d36b4cd
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-04-16 17:02:16 +0900

    푸터 완

--- src/component/layout/footer/index.tsx
+++ src/component/layout/footer/index.tsx
@@ -2,12 +2,13 @@ import { ReactComponent as KakaoIcon } from "../../../asset/svg/kakao.svg";
 import { ReactComponent as InstagramIcon } from "../../../asset/svg/instagram.svg";
 import { ReactComponent as YoutubeIcon } from "../../../asset/svg/youtube.svg";
 import './footer.scss'
+import { Typography } from "@mui/material";
 
 export default function Footer() {
     return (
         <footer className="site-footer">
             <div className="footer-top">
-                <p className="footer-slogan">당신의 발걸음을 더 가볍게, 매일이 런웨이처럼.</p>
+                <Typography variant="h4" className="footer-slogan">당신의 발걸음을 더 가볍게, 매일이 런웨이처럼.</Typography>
                 <div className="footer-links">
                     <a href="/faq">자주 묻는 질문</a>
                     <a href="/shipping">배송 & 반품 안내</a>
@@ -15,8 +16,7 @@ export default function Footer() {
                     <a href="/privacy">개인정보처리방침</a>
                 </div>
                 <div className="footer-contact">
-                    <p>💬 카카오톡 채널: <a href="#">@starshoes</a></p>
-                    <p>📞 고객센터: 02-1234-5678</p>
+                    <p>💬 카카오톡 채널: <a href="#">@Starlightshoes</a></p>
                     <p>운영시간: 평일 10:00 - 17:00</p>
                 </div>
                 <div className="footer-social">
@@ -32,7 +32,12 @@ export default function Footer() {
                 </div>
             </div>
             <div className="footer-bottom">
-                <p>© 2025 Star Shoes. All rights reserved.</p>
+                <Typography variant="body2">
+                    © 2025 Starlight Shoes. All rights reserved.
+                </Typography>
+                <Typography variant="body2">
+                    본 웹사이트의 디자인, 코드, 이미지 등의 모든 콘텐츠는 Star Shoes의 자산이며, 사전 허가 없이 무단 복제, 배포, 사용을 금합니다.
+                </Typography>
             </div>
         </footer>
 
