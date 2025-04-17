e5ce0fcf85117d9f5cc40620eaa33504159ccfc3
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-04-17 14:40:46 +0900

    eslintrc 추가

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+        project: './tsconfig.json',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    plugins: [
+        'react',
+        'react-hooks',
+        'jsx-a11y',
+        '@typescript-eslint'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        'jsx-a11y/anchor-is-valid': 'off', // <a href="#"> 사용 허용
+        'react/react-in-jsx-scope': 'off', // Next.js 쓸 경우 필수
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+    },
+};
