bfdfd21f7ce303a4058e3d770676299fde14fafe
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-04-18 16:59:52 +0900

    페이지 올리기 버튼

---
+++ src/component/pageup/index.tsx
@@ -0,0 +1,5 @@
+export default function Pageup() {
+    return (
+        <div>Pageup</div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './asset/style/global.css'
-import { BrowserRouter } from 'react-router-dom';
+import Pageup from 'component/pageup';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Pageup />
     </BrowserRouter>
   </React.StrictMode>
 );
