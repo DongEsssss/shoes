fa5a264963da52f1e7fe9d64e9eb390ebeab669e
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-04-18 16:20:39 +0900

    회사 페이지 업데이트

--- src/pages/home/section/company/company.tsx
+++ src/pages/home/section/company/company.tsx
@@ -1,16 +1,14 @@
-import * as React from "react"
 import "./style.scss"
 import companydata from '../../../../asset/data/company.json';
 
 export default function Company() {
     const items = companydata.distributors
-
     return (
         <div className="carousel-wrapper">
             <div className="carousel-track">
                 {items.concat(items).map((item, i) => (
                     <div key={i} className="carousel-slide">
-                        <img src={item.logo} alt={item.name} style={{ width: '180px' }} />
+                        <img src={item.logo} alt={item.name} style={{ width: '150px' }} />
                     </div>
                 ))}
             </div>
