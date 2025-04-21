5f6267b06e6411bffa3009f5b5ed1896edc71bf9
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-04-21 09:28:06 +0900

    page up 추가

--- src/component/pageup/index.tsx
+++ src/component/pageup/index.tsx
@@ -1,5 +1,37 @@
+import ArrowCircleUpOutlinedIcon from '@mui/icons-material/ArrowCircleUpOutlined';
+
+import './pageup.scss';
+import { useEffect, useState } from 'react';
+
 export default function Pageup() {
+    const [showButton, setShowButton] = useState(false);
+    useEffect(() => {
+        const handleShowButton = () => {
+            if (window.scrollY > 500) {
+                setShowButton(true)
+            } else {
+                setShowButton(false)
+            }
+        }
+
+        window.addEventListener("scroll", handleShowButton)
+        return () => {
+            window.removeEventListener("scroll", handleShowButton)
+        }
+    }, [])
+
+    const PageUpButton = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
     return (
-        <div>Pageup</div>
+        <div className="pageup">
+            {showButton && (
+                <ArrowCircleUpOutlinedIcon onClick={PageUpButton} className="pageup-button" />
+            )}
+        </div>
     )
 }
