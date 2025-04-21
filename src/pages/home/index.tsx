afb066d827776db2012c67a71872bc6b81f93aba
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-04-21 15:33:48 +0900

    aos업데이트

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,34 @@
+import React, { useEffect } from "react";
+
+// aos 라이브러리 추가
+
 import Mainslider from './mainslider'
 import Company from './section/company/company'
 import Caterogy from './section/category'
 import MonthNewItem from './section/monthnewitem'
 import MonthBestItem from './section/monthbest'
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 export default function Home() {
+
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
     return (
         <>
             <Mainslider />
-            <Caterogy />
-            <MonthBestItem />
-            <MonthNewItem />
+            <div data-aos="fade-up">
+                <Caterogy />
+            </div>
+            <div data-aos="fade-left">
+                <MonthBestItem />
+            </div>
+            <div data-aos="fade-right">
+                <MonthNewItem />
+            </div>
             <Company />
         </>
     )
