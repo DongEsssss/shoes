23c8a991c435c7920f4537c4d55c72cdc0b14fac
임동균 <70128434+DongEsssss@users.noreply.github.com>
2025-04-24 13:38:21 +0900

    타입 추가 및 스타일 수정

---
+++ src/asset/type/notice.ts
@@ -0,0 +1,15 @@
+interface noitce {
+    id: string;
+    title: string;
+    content: string;
+    category: string;
+    image?: string;
+    createdAt: string;
+    updatedAt: string;
+    isPinned: boolean;
+    visible: boolean;
+}
+
+export type {
+    noitce
+}
